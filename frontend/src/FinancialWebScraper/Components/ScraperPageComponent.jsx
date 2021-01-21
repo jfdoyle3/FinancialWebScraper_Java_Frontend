ba97daa382f1b1e1ca0db79a3837c958865164f7
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-01-21 16:43:04 -0500

    working on spinner

--- frontend/src/FinancialWebScraper/Components/ScraperPageComponent.jsx
+++ frontend/src/FinancialWebScraper/Components/ScraperPageComponent.jsx
@@ -10,59 +10,71 @@ class ScraperPage extends Component {
   super(props);
   this.state={
     stocks: [],
+    loading: false
   };
   this.scrape = this.scrape.bind(this);
 }
 componentDidMount() {
-  // StockDataServices.retrieveStocks().then(
-  //   (response) =>
-  //   this.setState({ stocks: response.data }));
+  StockDataServices.retrieveStocks().then(
+    (response) =>
+    this.setState({ stocks: response.data,loading: false }));
 }
 scrape() {
 }
     render() {
-      return (
+      let data;
+      if (this.state.loading){
+        data=<p>Loading</p>
+      } else{
+        data=         
         <main>
         <div className="container">
-          <table id="topfive" className="table">
-            <thead>
+        <table id="topfive" className="table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Symbol</th>
+              <th>Last Price</th>
+              <th>Price Change</th>
+              <th>Change %</th>
+              <th>Market Time</th>
+              <th>Volume</th>
+              <th>Avg Vol</th>
+              <th>Market Cap</th>  
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.stocks.map((stock) => (
               <tr>
-                <th>Date</th>
-                <th>Symbol</th>
-                <th>Last Price</th>
-                <th>Price Change</th>
-                <th>Change %</th>
-                <th>Market Time</th>
-                <th>Volume</th>
-                <th>Avg Vol</th>
-                <th>Market Cap</th>  
+                <td>{stock.datestamp}</td>
+                <td>{stock.symbol}</td>
+                <td>{stock.lastPrice}</td>
+                <td>{stock.priceChange}</td>
+                <td>{stock.changePercentage}</td>
+                <td>{stock.marketTime}</td>
+                <td>{stock.volume}</td>
+                <td>{stock.avgvol}</td>
+                <td>{stock.marketCap}</td>
               </tr>
-            </thead>
-            <tbody>
-              {this.state.stocks.map((stock) => (
-                <tr>
-                  <td>{stock.datestamp}</td>
-                  <td>{stock.symbol}</td>
-                  <td>{stock.lastPrice}</td>
-                  <td>{stock.priceChange}</td>
-                  <td>{stock.changePercentage}</td>
-                  <td>{stock.marketTime}</td>
-                  <td>{stock.volume}</td>
-                  <td>{stock.avgvol}</td>
-                  <td>{stock.marketCap}</td>
-                </tr>
-                 ))}
-            </tbody>
-          </table>
-        </div>
-        <div>
-          <button  onClick={() => this.scrape()}>Scrape</button>
-        </div>
-        <div>
-      <Spinner animation="border" role="status">
-        <span className="sr-only">Loading...</span>
-      </Spinner>
-    </div >
+               ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <button  onClick={() => this.scrape()}>Scrape</button>
+      </div>
+      <div>
+    <Spinner animation="border" role="status">
+      <span className="sr-only">Loading...</span>
+    </Spinner>
+  </div >
+  </main>
+
+      }
+      
+      return (
+        <main>
+          {data}
       </main>
     );
   }
